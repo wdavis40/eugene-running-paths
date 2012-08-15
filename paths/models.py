d9f0cd43d47b5214539488432a8a1250d0f144c8
Wyatt Davis <wdavis40@email.arizona.edu>
2012-08-14 19:25:16 -0700

    Added new model

---
+++ paths/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Path(models.Model):
+  name = models.CharField(max_length=30)
+  city = models.CharField(max_length=30)
+  state_province = models.CharField(max_length=30)
+
+  def __unicode__(self):
+    return self.name
