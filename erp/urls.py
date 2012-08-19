81cbc70c3b7de3807e53f9db6f358bddd1c944c9
Wyatt Davis <wdavis40@email.arizona.edu>
2012-08-18 21:53:25 -0700

    Added admin app

--- erp/urls.py
+++ erp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from erp.views import home, paths, about
+from django.contrib import admin
+admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,8 +16,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    ('^$', home),
-    ('^paths/$', paths),
-    ('^about/$', about)
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home),
+    url(r'^paths/$', paths),
+    url(r'^about/$', about),
+
 )
