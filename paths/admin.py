c054f013fa53fb63a3f1e17056456ebfd8ceb09b
Wyatt Davis <wdavis40@email.arizona.edu>
2012-08-21 17:36:01 -0700

    Added State and Surface Models

--- paths/admin.py
+++ paths/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from paths.models import Path, City
+from paths.models import Path, City, State, Surface
 
 admin.site.register(Path)
 admin.site.register(City)
+admin.site.register(State)
+admin.site.register(Surface)
--- paths/models.py
+++ paths/models.py
@@ -6,12 +6,26 @@ class Path(models.Model):
   name = models.CharField(max_length=30)
   city = models.ForeignKey('City')
   state_province = models.CharField(max_length=30)
+  surface = models.ForeignKey('Surface', null=True)
 
   def __unicode__(self):
     return self.name
 
 class City(models.Model):
   name = models.CharField(max_length=30)
+  state = models.ForeignKey('State', null=True)
 
   def __unicode__(self):
     return self.name
+
+class State(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __unicode__(self):
+     return self.name
+
+class Surface(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __unicode__(self):
+     return self.name
